77baa85598c80521392604555cf0eba623843993
enxi14 <36089987+enxi14@users.noreply.github.com>
2023-11-11 12:57:10 -0500

    commit

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 function validateAndSubmit() {
-    // Retrieve phone number input
+    // Retrieve input values
+    var email = document.getElementById('email').value;
     var phoneNumber = document.getElementById('phone').value;
+    var name = document.getElementById('name').value;
+    var comments = document.getElementById('comments').value;
 
-    // Check if the phone number is not blank
-    if (phoneNumber.trim() === '') {
-        alert('Phone number cannot be blank.');
+    // Check if any field is blank
+    if (email.trim() === '' || phoneNumber.trim() === '' || name.trim() === '' || comments.trim() === '') {
+        alert('All fields are required.');
         return;
     }
 
@@ -14,60 +17,32 @@ function validateAndSubmit() {
         return;
     }
 
-    // Sanitize phone number
-    var sanitizedPhoneNumber = sanitizePhoneNumber(phoneNumber);
+    // Sanitize inputs to defend against XSS
+    var sanitizedEmail = sanitizeInput(email);
+    var sanitizedPhoneNumber = sanitizeInput(phoneNumber);
+    var sanitizedName = sanitizeInput(name);
+    var sanitizedComments = sanitizeInput(comments);
 
-    // Update the input field with the sanitized value
+    // Update the input fields with the sanitized values
+    document.getElementById('email').value = sanitizedEmail;
     document.getElementById('phone').value = sanitizedPhoneNumber;
+    document.getElementById('name').value = sanitizedName;
+    document.getElementById('comments').value = sanitizedComments;
 
     // Now, you can submit the form
     submitForm();
 }
 
-function sanitizePhoneNumber(phoneNumber) {
-    // Remove non-numeric characters other than dashes
-    var sanitizedPhoneNumber = phoneNumber.replace(/[^\d-]/g, '');
-
-    // Remove leading and trailing dashes
-    sanitizedPhoneNumber = sanitizedPhoneNumber.replace(/^-+|-+$/g, '');
-
-    // Ensure there is at least one digit
-    if (!/\d/.test(sanitizedPhoneNumber)) {
-        alert('Invalid phone number. Please include at least one digit.');
-        return '';
-    }
-
-    // Remove consecutive dashes
-    sanitizedPhoneNumber = sanitizedPhoneNumber.replace(/-{2,}/g, '-');
-
-    // Limit consecutive digits to 10 for better formatting
-    sanitizedPhoneNumber = sanitizedPhoneNumber.replace(/\d{11,}/g, '');
-
-    // Escape HTML characters to defend against XSS
-    sanitizedPhoneNumber = escapeHtml(sanitizedPhoneNumber);
-
-    return sanitizedPhoneNumber;
-}
-
-function escapeHtml(text) {
-    var map = {
-        '&': '&amp;',
-        '<': '&lt;',
-        '>': '&gt;',
-        '"': '&quot;',
-        "'": '&#039;'
-    };
-
-    return text.replace(/[&<>"']/g, function (m) { return map[m]; });
+function sanitizeInput(input) {
+    // Implement proper input sanitization logic to defend against XSS
+    // For simplicity, this example only replaces '<' and '>' characters
+    return input.replace(/</g, '&lt;').replace(/>/g, '&gt;');
 }
 
 function submitForm() {
     // Perform any additional form submission logic here
     // For example, you can use AJAX to send the form data to the server
     // and handle the response accordingly.
-
-    // For demonstration purposes, alert the sanitized phone number
-    alert('Sanitized Phone Number: ' + document.getElementById('phone').value);
-
+    alert('Form submitted successfully!');
     // Add your form submission logic here
 }
